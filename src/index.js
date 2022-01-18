2cb7fbaa18af60d9c836aee5227568a901d4ab39
Paul Sokin <sorkinpavel@mail.ru>
2022-01-18 10:06:26 +0500

    init: create the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let stringN= Math.abs(n).toString();
+  let stringResult = '';
+
+  for (let i = 0; i < stringN.length; i += 1) {
+      stringResult = `${stringN[i]}${stringResult}`;
+  }
+  let result = Number(stringResult);
+
+  return(result);
 }
